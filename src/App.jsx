faf4b8f2ca019be84a29a97295ee9deb1915ce27
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2022-08-29 16:08:04 -0400

    added all logic

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,27 @@ function App() {
   const [rating, setRating] = useState(null)
 
   const ratingScale = {
-    1: 'TOO WEAK', 2: 'TOO WEAK', 3: 'WEAK', 4: 'MEDIUM', 5: 'STRONG', test: 'checkmark'
+    1: { name: 'TOO WEAK!', color: 'red' },
+    2: { name: 'TOO WEAK!', color: 'red' },
+    3: { name: 'WEAK', color: 'orange' },
+    4: { name: 'WEAK', color: 'orange' },
+    5: { name: 'MEDIUM', color: 'yellow' },
+    6: { name: 'MEDIUM', color: 'yellow' },
+    7: { name: 'STRONG', color: 'green' },
+    8: { name: 'STRONG', color: 'green' }
   }
 
   function updateSettings(e) {
     setSettings({ ...settings, [e.target.name]: e.target.checked })
   }
 
+  //global string values to use in generating a password
   const lowercase = 'abcdefghijklmnopqrstuvwxyz'
   const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   const numbers = '1234567890'
   const symbols = '!@#$%^&*()'
 
-  const checkLower = /[a-z]/
-  const checkSpecial = /[!@#$%^&*()]/
-  const checkNumber = /[0-9]/
-  const checkUpper = /[A-Z]/
-
+  //evaluate which options the user has checked, and concat global string values to set password characters
   function setString() {
     let passString = ''
     if (settings.upper) { passString = passString + uppercase }
@@ -35,6 +39,8 @@ function App() {
     if (settings.symbols) { passString = passString + symbols }
     return passString
   }
+
+  //accepts a number value and a string to generate a random set of characters form the string
   function makePassword(num, string) {
     let password = []
     for (let i = 0; i < num; i++) {
@@ -43,6 +49,8 @@ function App() {
     }
     return (password.join(''))
   }
+
+  //validate that the user has made a selection, then call each function to generate the password
   function checkAndGenerate() {
     if (slider === 0 || !settings.upper && !settings.lower && !settings.numbers && !settings.symbols) {
       return alert('Adjust selections and try again')
@@ -53,21 +61,25 @@ function App() {
     ratePassword(password)
   }
 
+  //evaluate the generated password and assign a number rating value
   function ratePassword(password) {
-    let rating = 0
+    const checkLower = /[a-z]/
+    const checkSpecial = /[!@#$%^&*()]/
+    const checkNumber = /[0-9]/
+    const checkUpper = /[A-Z]/
+    let rating = 1
     if (checkUpper.test(password)) { rating = rating + 0.5 }
     if (checkLower.test(password)) { rating = rating + 0.5 }
-    if (checkNumber.test(password)) { rating = rating + 0.5 }
-    if (checkSpecial.test(password)) { rating = rating + 0.5 }
-    if (password.length >= 3) { rating = rating + 0.5 }
-    if (password.length >= 6) { rating = rating + 0.5 }
-    if (password.length >= 9) { rating = rating + 0.5 }
-    if (password.length >= 12) { rating = rating + 0.5 }
-    if (password.length >= 15) { rating = rating + 0.5 }
-    if (password.length >= 18) { rating = rating + 0.5 }
+    if (checkNumber.test(password)) { rating = rating + 1 }
+    if (checkSpecial.test(password)) { rating = rating + 1 }
+    if (password.length >= 5) { rating = rating + 0.5 }
+    if (password.length >= 8) { rating = rating + 0.5 }
+    if (password.length >= 11) { rating = rating + 0.5 }
+    if (password.length >= 15) { rating = rating + 0.8 }
+    if (password.length >= 18) { rating = rating + 0.8 }
+    if (password.length >= 20) { rating = rating + 1 }
     setRating(Math.floor(rating))
   }
-
 
   return (
     <>
@@ -78,7 +90,10 @@ function App() {
         <div className="sub-container">
           <section className="password-display flex justify">
             <p>{!password ? 'P4$5W0rD!' : password}</p>
-            <img onClick={() => console.log('img click')} src={copyIMG} alt="copy" />
+            <div className='flex gap10'>
+              <p className='copied'>COPIED</p>
+              <img className='copy-password' onClick={() => navigator.clipboard.writeText(password)} src={copyIMG} alt="copy" />
+            </div>
           </section>
         </div>
         <div className="sub-container flex flex-col">
@@ -111,13 +126,22 @@ function App() {
           </section>
           <section className="strength-check flex justify">
             <p>Strength</p>
-            {!rating ? <p>'none'</p> : <p>{ratingScale[rating]}</p>}
+            <div className='flex'>
+              {!rating ? null : <p>{ratingScale[rating].name}</p>}
+              <div className='flex'>
+                <div className={!rating ? 'rate-box' : 'rate-box ' + ratingScale[rating].color}></div>
+                <div className={!rating || rating < 3 ? 'rate-box' : 'rate-box ' + ratingScale[rating].color}></div>
+                <div className={!rating || rating < 5 ? 'rate-box' : 'rate-box ' + ratingScale[rating].color}></div>
+                <div className={!rating || rating < 7 ? 'rate-box' : 'rate-box ' + ratingScale[rating].color}></div>
+              </div>
+            </div>
+
           </section>
           <section className="generate">
             <button onClick={checkAndGenerate}>GENERATE<img className='arrow-right' src={arrowimg} alt="arrow right" /></button>
           </section>
         </div>
-      </div>
+      </div >
     </>
 
   )
@@ -125,3 +149,11 @@ function App() {
 
 export default App
 
+//display four boxes
+//set color of boxes by rating
+//too weak, first is red
+//weak, two are orange
+//medium, three are yellow
+//strong, four are green
+
+//create one box and set color by rating
