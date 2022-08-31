3061dfb1ba4895f72bc51cdda2c3464148ed7d51
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2022-08-31 09:25:29 -0400

    correct slider colors on selection

--- src/components/PasswordDisplay.jsx
+++ src/components/PasswordDisplay.jsx
@@ -4,10 +4,10 @@ export default function PasswordDisplay({ password, copied, copyPassword }) {
 
     return (
         <section className="password-display flex justify">
-            <p className={!password ? 'inactive password' : 'password'}>{!password ? 'P4$5W0rD!' : password}</p>
+            <p role='show generated password' className={!password ? 'inactive password' : 'password'}>{!password ? 'P4$5W0rD!' : password}</p>
             <div className='copy-container flex'>
                 <p className={copied}>COPIED</p>
-                <img onClick={copyPassword} src={copyIMG} alt="copy" />
+                <img role='use to copy password' onClick={copyPassword} src={copyIMG} alt="copy icon" />
             </div>
         </section>
     )
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,13 @@
 export default function Slider({ slider, setSlider }) {
+
+    function handleChange(e) {
+        /*set slider vlaue*/
+        setSlider(e.target.value)
+        /*set background color of slider to trailing green*/
+        let value = (e.target.value / e.target.max) * 100
+        e.target.style.background = `linear-gradient(to right, #A4FFAF ${value}%, #18171F ${value}%)`
+    }
+
     return (
         <section className="length-select flex flex-col">
             <div className='char-display flex justify'>
@@ -6,8 +15,10 @@ export default function Slider({ slider, setSlider }) {
                 <p className='charnum'>{slider}</p>
             </div>
             <div>
-                <input type="range" value={slider} onChange={(e) => setSlider(e.target.value)} id='charnum' name='charnum' min='0' max='20' step='1' />
+                <label htmlFor="charnum" id="charnumlabel">Character Length Select</label>
+                <input type="range" value={slider} onChange={(e) => handleChange(e)} id='charnum' name='charnum' min='0' max='20' step='1' />
             </div>
         </section>
     )
 }
+
